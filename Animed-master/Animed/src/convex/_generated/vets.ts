6dd29b718b1b44b55d6f98a21d56fdf25e60b911
Adrian Henrique Olinek da Silva <162664330+adrianhenrique17@users.noreply.github.com>
2025-11-17 11:48:05 -0300

    Delete Animed-master directory

--- Animed-master/Animed/src/convex/_generated/vets.ts
+++
@@ -1,5 +0,0 @@
-// src/vets.ts
-
-// O caminho relativo a partir de 'src' para 'api-animed'
-import vetsData from "@api/data/services.json";
-export default vetsData;
--- Animed-master/api-animed/src/models/UserModel.js
+++
@@ -1,8 +0,0 @@
-export default class User {
-  constructor({ id, name, email, password }) {
-    this.id = Number(id);
-    this.name = name;
-    this.email = email;
-    this.password = password;
-  }
-}
