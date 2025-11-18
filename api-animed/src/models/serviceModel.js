27b9a9fd21343cb6103ca9bfe504353f69f85bbf
Adrian Henrique Olinek da Silva <162664330+adrianhenrique17@users.noreply.github.com>
2025-11-17 23:18:43 -0300

    Delete api-animed directory

--- api-animed/src/models/serviceModel.js
+++
@@ -1,9 +0,0 @@
-export default class Service {
-  constructor({ id, vetName, especialidade = "", serviceName, price }) {
-    this.id = Number(id);
-    this.vetName = vetName;
-    this.especialidade = especialidade;
-    this.serviceName = serviceName;
-    this.price = Number(price);
-  }
-}
