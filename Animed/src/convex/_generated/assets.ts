66fd5b1eff9a1d9ef28f36b36349a1a0081ddd60
Adrian Henrique Olinek da Silva <162664330+adrianhenrique17@users.noreply.github.com>
2025-11-17 23:18:35 -0300

    Delete Animed directory

--- Animed/src/convex/_generated/assets.ts
+++
@@ -1,8 +0,0 @@
-export const Logo1 = require("../../img/Logo1.png");
-export const Logo2 = require("../../img/Logo2.png");
-export const Logo3 = require("../../img/LogoD3.png");
-export const Logo4 = require("../../img/Logo4D.png");
-export const ModoClaro = require("../../img/claro.png");
-export const ModoEscuro = require("../../img/escuro.png");
-
-// Adicione todas as imagens que você usa
